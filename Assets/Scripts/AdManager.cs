c4d96950c8cdded04adbf84180d558cd20f27c97
Lewis Nicholson <Lewies@Live.co.uk>
2019-11-03 16:52:35 +0000

    Starting Ads work

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : Singleton<AdManager>
+{
+    [SerializeField] private AdProvider adProvider;
+
+}
